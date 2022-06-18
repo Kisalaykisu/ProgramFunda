8a1b85d90c0a846afc582d110c428c7789f41d31
Kisalay <kisalaykisu@gmail.com>
2022-06-18 13:15:27 +0530

    first commit

---
+++ IdeaProjects/WageEmployee/src/com/bridgelabz/WageEmployee.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class WageEmployee {
+    public static void main(String args[]) {
+        //Welcome Message.
+        System.out.println("Welcome into the playground of coding,Wage computation is the first one.");
+    }
+}
