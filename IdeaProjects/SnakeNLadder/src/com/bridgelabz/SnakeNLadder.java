5d16b161db0a1d01a0da170c1de25d47720cfc51
Kisalay <kisalaykisu@gmail.com>
2022-06-19 21:55:17 +0530

    first commit

---
+++ IdeaProjects/SnakeNLadder/src/com/bridgelabz/SnakeNLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeNLadder {
+    public static void main(String[] args) { // Entry point of Program
+
+        System.out.println("Welcome to the Game of Snake and Ladder. Lets play the game");
+    }
+}
